44360508e9b4df5dec4afadd3a90c57066839edc
hoseon07 <a01081930429@gmail.com>
2023-12-01 15:01:16 +0900

    👀 :: chore :: Signup 변경

--- reactapp/src/pages/auth/SignUp.jsx
+++ reactapp/src/pages/auth/SignUp.jsx
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { useState }from "react";
 import styled from "styled-components";
 import AuthTitle from "../../components/common/AuthTitle";
 import AuthButton from "../../components/common/AuthButton";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 /**
  * @return 회원가입
  */
 
 export default function SignUp() {
+
   return (
     <>
       <Container>
         <AuthTitle title="회원가입" />
         <InputBox>
-          <Input type="text" placeholder="아이디를 입력하세요" />
-          <Input type="password" placeholder="비밀번호를 입력하세요" />
-          <Input type="password" placeholder="비밀번호를 재입력하세요" />
-          <Input type="text" placeholder="이름을 입력하세요" />
+          <Input 
+            type="text" 
+            placeholder="이름을 입력하세요" 
+          />
           <Email>
             <EmailInput type="email" placeholder="이메일을 입력하세요" />
             <Selection>
               <Option value={"gmail"}>@gmail.com</Option>
               <Option value={"naver"}>@naver.com</Option>
               <Option value={"daum"}>@daum.com</Option>
+              <Option value={"user"}>사용자 지정</Option>
             </Selection>
           </Email>
+          <Input 
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+          />
+          <Input 
+            type="password"
+            placeholder="비밀번호를 재입력하세요" 
+          />
           <Input type="text" placeholder="인증번호를 입력하세요" />
-          <AuthButton text="가입하기" />
+          <AuthButton text="가입하기"/>
         </InputBox>
       </Container>
     </>
@@ -70,7 +82,7 @@ const EmailInput = styled.input`
   margin-bottom: 25px;
   padding-left: 20px;
   display: inline-block;
-  width: 240px;
+  width: 290px;
   height: 40px;
   font-size: 16px;
   font-weight: 500;
@@ -88,7 +100,7 @@ const Selection = styled.select`
   padding: 0px 10px;
   display: inline-block;
   margin: 0px 0px 0px 10px;
-  width: 200px;
+  width: 210px;
   height: 40px;
   font-size: 16px;
   font-weight: 500;
