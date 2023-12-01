310610cf0f018d9999ee69e849d8523913d70fe3
juwon <juwon2298@naver.com>
2023-12-01 15:07:49 +0900

    ✍ :: style & refactor :: 코드 스타일 변경
    styled-component 위치 변경

--- reactapp/src/components/common/Top.jsx
+++ reactapp/src/components/common/Top.jsx
@@ -2,26 +2,7 @@ import styled from "styled-components";
 import SearchBox from "./SearchBox";
 import MyPageBtn from "./MyPageBtn";
 
-export const FlexDiv = styled.div`
-  display: flex;
-  flex-direction: ${({ isColumn }) => (isColumn ? "column" : "row")};
-  align-items: ${({ alignDir }) => (alignDir ? alignDir : "center")};
-`;
 const Top = ({ myBtnText }) => {
-  const BtnNInputs = styled(FlexDiv)`
-    width: 910px;
-    margin: 45px 0px 0px 0px;
-    justify-content: space-between;
-  `;
-  const Tip = styled.p`
-    color: #707070;
-    margin: 10px 10px;
-    font-family: Inter, sans-serif;
-    font-size: 15px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-  `;
   return (
     <FlexDiv isColumn={true} alignDir={"flex-start"}>
       <BtnNInputs>
@@ -32,5 +13,27 @@ const Top = ({ myBtnText }) => {
     </FlexDiv>
   );
 };
+
+export const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: ${({ isColumn }) => (isColumn ? "column" : "row")};
+  align-items: ${({ alignDir }) => (alignDir ? alignDir : "center")};
+`;
+
+const BtnNInputs = styled(FlexDiv)`
+width: 910px;
+margin: 45px 0px 0px 0px;
+justify-content: space-between;
+`;
+
+const Tip = styled.p`
+color: #707070;
+margin: 10px 10px;
+font-family: Inter, sans-serif;
+font-size: 15px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
+`;
 
 export default Top;
